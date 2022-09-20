891c56df13de566a70badb452a5a4b6aeb6fcdf5
Sagar <sagarverma5185@gmail.com>
2022-09-20 14:40:38 +0530

    create user sign up

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.users);
 
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
+
+router.post('/create', usersController.create);
+
 module.exports = router;
