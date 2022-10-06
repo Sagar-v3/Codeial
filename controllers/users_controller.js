384708c3280c88674639b41944ef20539030d38a
Sagar <sagarverma5185@gmail.com>
2022-10-06 12:33:15 +0530

    Authentication using Passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,6 +9,10 @@ module.exports.users = function(req,res) {
 
 // render the sign up page
 module.exports.signUp = function(req, res) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
@@ -16,6 +20,10 @@ module.exports.signUp = function(req, res) {
 
 // render the sign in page
 module.exports.signIn = function(req, res) {
+    if(req.isAuthenticated()) {
+       return  res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     });
@@ -46,6 +54,16 @@ module.exports.create = function(req, res) {
     });
 }
 
+// sign in and create a session for the user
 module.exports.createSession = function(req, res) {
     // TODO later
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res) {
+    console.log(req);
+    req.logout();
+
+    
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -16,12 +22,38 @@ app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
 
-// use express router
-app.use('/', require('./routes'));
-
 // set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deploying in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+        mongooseConnection: db,
+        autoRemove: 'disabled'
+    },
+    function(err){
+        console.log(err || 'connect-mongodb setup ok');
+    }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err) {
     if(err) {
