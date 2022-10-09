12d0b726e9625d87b45b7fe8288862085c39c3eb
Sagar <sagarverma5185@gmail.com>
2022-10-09 05:41:06 +0530

    Save Posts to the DB

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts',require('./posts'));
 
 
 
-module.exports = router;
+module.exports = router;                                                
