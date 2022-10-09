e87be0126292f2ef1a0615ed862e4c8c6bd05cbf
Sagar <sagarverma5185@gmail.com>
2022-10-09 06:17:33 +0530

    Display Posts and releated User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,20 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res) {
 
-    return res.render('home', {
-        title: "Home"
+    // Post.find({}, function(err, posts) {
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
+   
 }
