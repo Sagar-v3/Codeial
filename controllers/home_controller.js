e12aa942e4cc1d5225dec4295d970f332a4ba441
Sagar <sagarverma5185@gmail.com>
2022-10-10 07:06:27 +0530

    Display comments on post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,15 @@ module.exports.home = function(req, res) {
     // });
 
     // Populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
