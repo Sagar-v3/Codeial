bc9f97345d8e310b11e2f8fe37b4371a7b09ae1c
Sagar <sagarverma5185@gmail.com>
2022-10-11 11:56:35 +0530

    Deleting a Comment

--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const commentssController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication, commentssController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentssController.destroy);
 
 module.exports = router;
